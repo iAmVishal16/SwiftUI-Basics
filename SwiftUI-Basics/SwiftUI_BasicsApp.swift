ee1d637a4d39b4350de78622da59bae5d272583d
iamvishal16 <34365396+iAmVishal16@users.noreply.github.com>
2024-04-19 21:31:44 +0530

    Initial Commit

---
+++ SwiftUI-Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI-Basics
+//
+//  Created by Vishal Paliwal on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
